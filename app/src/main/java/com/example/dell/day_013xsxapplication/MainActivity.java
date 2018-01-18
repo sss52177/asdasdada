11888297f4677217d716800e85c660df1bf912aa
dell <3156727210@qq.com>
2018-01-18 19:14:25 +0800

    dasaaasasdas

---
+++ app/src/main/java/com/example/dell/day_013xsxapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dell.day_013xsxapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
